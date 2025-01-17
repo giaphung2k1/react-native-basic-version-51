edb9e0112bfee9a80f12bd63dc18486a3c4feffe
Hung <giaphung2k1@gmail.com>
2025-01-17 23:13:37 +0700

    video 55

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const AppRoot = () => {
+    return(
+        <View>
+            <Text>
+                Hello World
+            </Text>
+        </View>
+    )
+}
+
+export default AppRoot;
